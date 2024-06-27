1474e384c639f96b0cabf0f674a16d652c1ae947
zyj <zhangyongjun.123@bytedance.com>
2024-06-27 17:24:02 +0800

    feat :myzyy

---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    FrameName: ['express', 'koa', 'egg', 'uni'],
+    FrameNameUrls: {
+        express: 'git@gitee.com:gao-shunpeng/express-template.git',
+        koa: 'git@gitee.com:gao-shunpeng/koa-template.git',
+        egg: 'git@gitee.com:gao-shunpeng/egg-template.git',
+        uni: 'git@gitee.com:gao-shunpeng/egg-template.git',
+    }
+}
